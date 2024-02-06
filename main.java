a51df2f4ae527110ea1e4dbe4ae93e8a0653f8f0
SnailMail500 <alex_snell@icloud.com>
2024-02-06 13:51:38 +0000

    First commit

---
+++ main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main (String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Every println makes a new line.");
+        System.out.print("but print doesn't.");
+        System.out.println("see?");
+        //this is a comment, like in c++ and c
+        /*this is a multiline comment
+         * and I hate them, just like in c++ and c
+         */
+        //to make a variable, use type varName = val;
+        String hello = "helloWorld";
+        int num = 8;
+        char un = '1';
+        boolean yes = true;
+        System.out.println(hello);
+        System.out.println(num);
+        System.out.println(un);
+        System.out.println(yes);        
+        //to save memory, if the value, v meets the below condition and can be stored as an integer, it can be saved as a 'byte' data type. Cool.
+        //-128<v<127
+
+    }
+}
